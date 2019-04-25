30a976daedeb7611a731dd99bddf3327a12c1edc
webmaster0307 <web.master0307@gmail.com>
2019-04-25 17:11:44 +0800

    adding relationships

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+       return $this->hasMany(Question::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
