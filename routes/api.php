119a5a6c7d926b038376442821a2b5d843417ea7
webmaster0307 <web.master0307@gmail.com>
2019-04-29 14:19:22 +0800

    implementation JWT inside Laravel

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();});
+Route::group([
 
-Route::apiResource('/question','QuestionController');
-Route::apiResource('/category','CategoryController');
-Route::apiResource('/question/{question}/reply','replyController');
-Route::post('/like/{reply}','LikeController@likeIt');
-Route::delete('/like/{reply}','LikeController@unLikeIt');
+    'middleware'=> 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::apiResource('/question','QuestionController');
+    Route::apiResource('/category','CategoryController');
+    Route::apiResource('/question/{question}/reply','replyController');
+    Route::post('/like/{reply}','LikeController@likeIt');
+    Route::delete('/like/{reply}','LikeController@unLikeIt');
+
+});
+
+
+
 
